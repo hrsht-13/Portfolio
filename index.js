d7dd4c5af84e8ba89bbe4edaba2f5e380f305d4a
Harshita Gupta <hrsht1998@gmail.com>
2021-12-20 17:07:21 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+const navToggle = document.querySelector('.nav-toggle');
+const navLinks = document.querySelectorAll('.navi_link');
+
+
+navToggle.addEventListener('click', () => {
+    document.body.classList.toggle('nav-open');
+});
+
+
+navLinks.forEach(link => {
+    link.addEventListener('click', () => {
+        document.body.classList.remove('nav-open');
+    })
+});
+
+function myFunction() {
+    var element = document.body;
+    element.classList.toggle("dark-mode");
+ };
